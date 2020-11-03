262ab1d94b3204ac858ea17d639fe81ac75af340
Alberte Laventure <alberte.laventure@gmail.com>
2020-11-02 21:56:44 -0500

    Added tabs to portfolio

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Header/>
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import NavTabs from "./components/NavTabs";
 
 
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <NavTabs />
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/portfolio" component={Portfolio} />
         
-        <Footer/>
-      </>
+        <Footer />
+      </div>
+      </Router>
+
     );
-  }
 }
 
 export default App;
