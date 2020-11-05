fa939ae533898b204aa8900282f417d960c22453
Alberte Laventure <alberte.laventure@gmail.com>
2020-11-05 09:36:56 -0500

    fixing package json

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,7 +4,10 @@ import CardDeck from "../components/CardDeck/CardDeck";
 class Portfolio extends React.Component {
   render() {
     return (
+      <>
+      <h1>Portfolio</h1>
       <CardDeck />
+      </>
     );
   }
   }
