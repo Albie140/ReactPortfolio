cb0073b5a4bceb7bdf1554e1fc94689265efc66e
Alberte Laventure <alberte.laventure@gmail.com>
2020-11-04 23:07:58 -0500

    Deployed to Github pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ function About() {
           <h1>About Me</h1>
           <img src={Me} alt="Me!" className="img-fluid" height={400} width={400}/>
               <p>
-              My name is Alberte, pronounced Albert, like Einstein. I was raised in Philadelphia, PA and I currently reside in Conshohocken, PA (just outside of Philadelphia).I graduated from Penn State University with a degree in Hospitality Management. After 10 years of working in the hospitality industry,  I decided it was time for a career change. I enjoy puzzles and challenges so I decided to work towards becoming a full stack web developer. Please check out my portfolio, where you can view some of the applications I have created. Feel free to reach out to me via the information found in the contact form.
+              My name is Alberte, pronounced Albert, like Einstein. I was raised in Philadelphia, PA and I currently reside in Conshohocken, PA (just outside of Philadelphia). I graduated from Penn State University with a degree in Hospitality Management. After 10 years of working in the hospitality industry,  I decided it was time for a career change. I enjoy puzzles and challenges so I decided to work towards becoming a full stack web developer. Please check out my portfolio, where you can view some of the applications I have created. Feel free to reach out to me via the information found in the contact form.
               </p>
           
       </div>
